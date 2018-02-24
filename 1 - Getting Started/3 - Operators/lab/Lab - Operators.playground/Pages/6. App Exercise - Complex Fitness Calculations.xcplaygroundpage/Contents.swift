ffa1abf84048a024ff067edaba1e7a5714626ac2
GMoney22 <garrettgriffith@icloud.com>
2018-02-24 17:41:32 -0600

    1.3 Complete

--- 1 - Getting Started/3 - Operators/lab/Lab - Operators.playground/Pages/6. App Exercise - Complex Fitness Calculations.xcplaygroundpage/Contents.swift
+++ 1 - Getting Started/3 - Operators/lab/Lab - Operators.playground/Pages/6. App Exercise - Complex Fitness Calculations.xcplaygroundpage/Contents.swift
@@ -5,6 +5,12 @@
  
  If you completed the Fitness Calculations exercise, you calculated an average heart rate to display to the user. However, using proper order of operations you can do this in fewer steps. Create three separate heart rate constants, all of type `Double`, with values between 60 and 100. Then create a constant equal to the average heart rate. If you use correct order of operations you can do the heart calculation in one line.
  */
+let heartRate1: Double = 60.0
+let heartRate2: Double = 80.0
+let heartRate3: Double = 100.0
+
+let averageHeartRate = (heartRate1 + heartRate2 + heartRate3) / 3
+print(averageHeartRate)
 
 
 /*:
@@ -12,6 +18,8 @@
  
  You may want to also show the temperature in celsius. You can convert fahrenheit to celsius by taking `tempInFahrenheit` and subtracting 32, then multiplying the result by (5.0/9.0). Create a constant `tempInCelsius` that calculates in one line the temperature in celsius.
  */
-
+let tempInFarenheit = 98.6
+let tempInCelcius = (tempInFarenheit - 32) * (5.0/9.0)
+print(tempInCelcius)
 
 //: [Previous](@previous)  |  page 6 of 8  |  [Next: Exercise - Numeric Type Conversion](@next)
